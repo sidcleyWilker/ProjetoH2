c3ba1d1ce5fe94a142c4cb2c65d201c50b95fb03
sidcleywilker <sidcleywilker@gmail.com>
2014-09-02 13:25:48 -0300

    salvando os ultimos teste

--- src/App.java
+++ src/App.java
@@ -1,15 +1,22 @@
 import padroesDeProjeto.BancoDeDados.BD;
 import padroesDeProjeto.BancoDeDados.IO;
 import padroesDeProjeto.Exception.H2Exception;
+import padroesDeProjeto.Fachada.FachadaCliente;
 
 
 public class App {
 
 	public static void main(String[] args) throws H2Exception {
 		
-		BD bd = new BD();
-		IO.getInstance().save(bd);
-		
+//		BD bd = new BD();
+//		IO.getInstance().save(bd);
+//		
+		FachadaCliente fa = new FachadaCliente();
+//" addDisciplinaAoPeriodo id="" nomeDaDisciplina="Padrões de Projeto" cargaHoraria=100 nomeCurso="ADS"  sigla="ADS_PP" 
+//id = "id" nome = "bd1" cargaHoraria 100 curso = "ADS" periodo="2012.1"
+		fa.addCurso("ADS", "Analise e de...");
+		fa.addPeriodo("2012.1", "ADS");
+		fa.addDisciplinaAoPeriodo("id","bd1", 100, "ADS", "2012.1");
 	}
 
 }
